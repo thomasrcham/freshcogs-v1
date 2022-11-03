ab24a50d10bd6fc2b79f773302ce7f5027375adf
thomasrcham <guseldorph@gmail.com>
2022-11-03 14:41:45 -0500

    fetch happens

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "./keys.js";
 
 import { NavigationContainer } from "@react-navigation/native";
 // import Home from "./components/Home";
 
 export default function App() {
   const [text, setText] = useState("Hallo");
+  const [albums, setAlbums] = useState({});
+
+  console.log(albums);
+
+  useEffect(() => {
+    fetch(
+      `https://api.discogs.com/users/theyear1000/collection/folders/0/releases?per_page=25${token}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let returnData = data.releases;
+        let parsedReleases = returnData.map((release) => parseInfo(release));
+        setAlbums(parsedReleases);
+      });
+  }, []);
+
+  function parseInfo(release) {
+    let parsedReleases = [];
+    let singleParsedRelease = {
+      artist: release.basic_information.artists[0].name,
+      title: release.basic_information.title,
+      cover: release.basic_information.cover_image,
+    };
+
+    parsedReleases.push(singleParsedRelease);
+
+    return parsedReleases;
+  }
+
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
@@ -15,7 +45,6 @@ export default function App() {
       </Button>
     </View>
   );
-  // return <Home />;
 }
 
 const styles = StyleSheet.create({
