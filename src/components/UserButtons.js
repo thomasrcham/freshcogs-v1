1049da40c7046ceb2e36efae094abce4c11fcbac
thomasrcham <guseldorph@gmail.com>
2022-12-25 19:46:54 -0600

    full album scrobbling works

--- src/components/UserButtons.js
+++ src/components/UserButtons.js
@@ -8,6 +8,7 @@ import {
   Text,
   TextInput,
   TouchableWithoutFeedback,
+  Alert,
 } from "react-native";
 import { useState, useEffect, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -62,72 +63,62 @@ export default function UserButtons({
     fetch(`https://api.discogs.com/${URL}`, requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        let r = tracklistFetch(result.tracklist);
-        let string = tracklistToString(r, album);
-        // console.log(string);
-        let md5 = tracklistToMD5(r, album);
+        let tracklist = tracklistFetch(result.tracklist);
+        let scrobbleInterval = setInterval(() => {
+          if (tracklist.length === 0) {
+            console.log("finished");
+            clearInterval(scrobbleInterval);
+          } else {
+            scrobbleTrack(tracklist[0], album);
+            tracklist.shift();
+          }
+        }, 10000);
       })
       .catch((e) => console.log(e));
+  };
+
+  var CryptoJS = require("crypto-js");
+  var parseString = require("xml2js").parseString;
+
+  const scrobbleTrack = (singleTrack, album) => {
+    let dateTime = Math.round(Date.now() / 1000);
+    let md5String = CryptoJS.MD5(
+      `album[0]${album.title}api_key${lfm_api_key}artist[0]${album.artist}methodtrack.scrobblesk${LFMKey}timestamp[0]${dateTime}track[0]${singleTrack.title}${lfm_secret}`
+    ).toString();
+    let fullString = `method=track.scrobble&artist[0]=${album.artist}&track[0]=${singleTrack.title}&timestamp[0]=${dateTime}&album[0]=${album.title}&api_key=${lfm_api_key}&sk=${LFMKey}&api_sig=${md5String}`;
+
+    var scrobbleRequestOptions = {
+      method: "POST",
+      redirect: "follow",
+    };
+
+    fetch(
+      `http://ws.audioscrobbler.com/2.0/?${fullString}`,
+      scrobbleRequestOptions
+    )
+      .then((response) => response.text())
+      .then((result) =>
+        parseString(result, function (err, output) {
+          output.lfm.scrobbles[0].$.accepted
+            ? console.log(
+                "scrobbled " + output.lfm.scrobbles[0].scrobble[0].track[0]._
+              )
+            : Alert.alert("failed");
+        })
+      )
+      .catch((error) => console.log("error", error));
   };
 
   const tracklistFetch = (tracklist) => {
     let tracklistArray = [];
     for (let i = 0; i < tracklist.length; i++) {
       let track = {
-        track: i,
+        trackNo: i,
         title: tracklist[i].title,
       };
       tracklistArray.push(track);
     }
     return tracklistArray;
-  };
-
-  const tracklistToString = (tracklistArray, album) => {
-    let fullAlbumString = [`method=track.scrobble&sk=${LFMKey}`];
-    for (let i = 0; i < tracklistArray.length; i++) {
-      let fullString = `artist[${i}]=${album.artist}&track[${i}]=${
-        tracklistArray[i].title
-      }&timestamp[${i}]=${Date.now() + i * 60000}&album[${i}]=${album.title}`;
-      fullAlbumString.push(fullString);
-    }
-    let combinedAlbumsString = fullAlbumString.join("&");
-    return combinedAlbumsString;
-  };
-
-  var CryptoJS = require("crypto-js");
-
-  const tracklistToMD5 = (tracklistArray, album) => {
-    let fullArtistMD5 = [];
-    let fullTrackMD5 = [];
-    let fullAlbumMD5 = [];
-    let fullTimestampMD5 = [];
-    for (let i = 0; i < tracklistArray.length; i++) {
-      let fullArtistString = `artist[${i}]${album.artist}`;
-      fullArtistMD5.push(fullArtistString);
-    }
-    console.log(fullArtistMD5);
-    for (let i = 0; i < tracklistArray.length; i++) {
-      let fullTrackString = `track[${i}]${tracklistArray[i].title}`;
-      fullTrackMD5.push(fullTrackString);
-    }
-
-    for (let i = 0; i < tracklistArray.length; i++) {
-      let fullAlbumString = `album[${i}]=${album.title}`;
-      fullAlbumMD5.push(fullAlbumString);
-    }
-    for (let i = 0; i < tracklistArray.length; i++) {
-      let fullTimestampString = `timestamp[${i}]=${Date.now() + i * 60000}`;
-      fullTimestampMD5.push(fullTimestampString);
-    }
-
-    let fullMD5 =
-      // CryptoJS.MD5(
-      `${fullAlbumMD5.join("")}api_key${lfm_api_key}${fullArtistMD5.join(
-        ""
-      )}methodtrack.scrobblesk${LFMKey}${fullTimestampMD5.join(
-        ""
-      )}${fullTrackMD5}${lfm_secret}`;
-    // ).toString();
   };
 
   return (
@@ -163,7 +154,10 @@ export default function UserButtons({
         <Button
           title="random tracklist"
           onPress={() =>
-            logEvent(albums.filter((a) => a.master_id === 11329)[0])
+            logEvent(
+              // albums.filter((a) => a.master_id === 11329)[0]
+              albums[Math.floor(Math.random() * albums.length)]
+            )
           }
         />
       </View>
